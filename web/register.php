f2873a152e52fd38c999d466c15ec39dca9bdbdb
Harlin <bangmay@gmail.com>
2015-09-17 23:55:20 +0900

    pleaseee

--- web/register.php
+++ web/register.php
@@ -1,9 +1,9 @@
 <?php 
-echo 'new';
+echo 'new2';
 $conn_string = "host=ec2-54-204-25-54.compute-1.amazonaws.com ";
 $conn_string .= "port=5432 dbname=d3j0nm9qghk5i6 user=etbyqbciasiion password= 2VoBZqhhBM1drFQtYCfltP434T";
 $dbconn = pg_connect($conn_string);
-$query = 'SELECT max(user) FROM public.user';
+$query = 'SELECT max(userid) FROM public.user';
 $rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
 
 while ($row = pg_fetch_row($rs)) $input_userid = $row[0];
