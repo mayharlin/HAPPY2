769fab0611b48406ddf93d04b8edfee611722c5b
jzyu <jzyu.ut@gmail.com>
2015-09-17 14:24:17 +0900

    modify hello

--- web/index.php
+++ web/index.php
@@ -22,6 +22,10 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
 
 $app->get('/cowsay', function() use($app) {
   $app['monolog']->addDebug('cowsay');
