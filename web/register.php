2a3d6c78eaccb4325cd5bda51d4b775dfd29e136
Harlin <bangmay@gmail.com>
2015-09-17 23:52:24 +0900

    pleaseee

--- web/register.php
+++ web/register.php
@@ -1,5 +1,5 @@
 <?php 
-$echo 'new';
+echo 'new';
 $conn_string = "host=ec2-54-204-25-54.compute-1.amazonaws.com ";
 $conn_string .= "port=5432 dbname=d3j0nm9qghk5i6 user=etbyqbciasiion password= 2VoBZqhhBM1drFQtYCfltP434T";
 $dbconn = pg_connect($conn_string);
