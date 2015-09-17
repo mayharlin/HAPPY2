183d56b74aa0f62e1d9e411d2d2bb4ed6e575e33
Harlin <bangmay@gmail.com>
2015-09-17 18:09:34 +0900

    again 1

--- web/profile.php
+++ web/profile.php
@@ -1,76 +1,145 @@
 <?php 
-
 $conn_string = "host=ec2-54-204-25-54.compute-1.amazonaws.com ";
 $conn_string .= "port=5432 dbname=d3j0nm9qghk5i6 user=etbyqbciasiion password= 2VoBZqhhBM1drFQtYCfltP434T";
 $dbconn = pg_connect($conn_string);
-
-print_r($dbconn);
-
-
-$query = 'SELECT username, "password", birthdate, address, phone FROM public.user LIMIT 5;'; 
-
+$query = 'SELECT username, birthdate, national, "password" FROM public.user LIMIT 5;';
 $rs = pg_query($dbconn, $query) or die("Cannot execute query: $query\n");
-
 ?>
 
-
+<!DOCTYPE html>
 <html>
+
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<link rel="stylesheet" href="css.css" media="screen" />
-<title><?php echo 'LP Invite Page' ?></title>
-<script src="jquery.min.js"></script>
-<script src="jquery-simple-pagination-plugin.js"></script>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-
+<title>Profile</title>
+<link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="profile.css">
 </head>
+
 <body>
-<h1><?php echo 'LP Invitation Page' ?></h1>
-<form action="home.php" method="post">
-	<?php while ($row = pg_fetch_row($rs)): ?>
-	<table>
-		<tr>
-			<td>Username</td>
-			<td><br/><input type="text" name="User" size="25" value="<?php echo $row[0]; ?>" /><br/><br/></td>
-		</tr>
-		<tr>
-			<td>Password</td>
-			<td><br/><input type="password" name="UserPass" size="25" value="<?php echo $row[1]; ?>" /><br/><br/></td>
-		</tr>
-		<tr>
-		<tr>
-			<td>FullName</td>
-			<td><br/><input type="text" name="FullName" size="25" value="<?php echo $row[0] . ' ' . $row[0]; ?>" /><br/><br/></td>
-		</tr>
-		<tr>
-			<td>Birth Day</td>
-			<td><br/><input type="text" name="Birthdate" size="25" value="<?php echo $row[2]; ?>" /><br/><br/></td>
-		</tr>
-		<tr>
-			<td>Address</td>
-			<td><br/><input type="text" name="Address" size="25" value="<?php echo $row[3]; ?>" /><br/><br/></td>
-		</tr>
-		<tr>
-			<td>Phone</td>
-			<td><br/><input type="text" name="Phone" size="25" value="<?php echo $row[4]; ?>" /><br/><br/></td>
-		</tr>
+<center>
+<form action="#" method="post">
+
+<h1>Profile</h1>
+
+<div class="information">
+<div class="container">
+
+<?php while ($row = pg_fetch_row($rs)): ?>
+<table>
+  <tr>
+    <td>Name</td>
+    <td><input id="Name" name="Name" type="text" style="width:200px" value="<?php echo $row[0]; ?>"></td>
+  </tr>
+	
+  <tr>
+    <td>Birthday</td>
+    <td><input id="Birthday" name="Birthday" type="text" style="width:200px" value="<?php echo $row[1]; ?>"></td>
+  </tr>
+	
+  <tr>
+    <td>Nationality</td>
+    <td><input id="nationality" name="Nationality" type="text" style="width:200px" value="<?php echo $row[2]; ?>"></td>
+  </tr>
 		
-		<tr>
-			<td></td>
-			<td><br/><input type="submit" value="Login" /><br/><br/></td>
-		</tr>
-	</table><br/>
-	<?php endwhile; ?>
+  <tr>
+    <td>Email Address</td>
+    <td><input id="Email" name="Email" type="text" style="width:200px" value="<?php echo $row[0]; ?>@rakuten.co.jp"></td>
+  </tr>
+
+	<tr>
+    <td>Password</td>
+    <td><input id="Password" name="Password" type="text" style="width:200px" value="<?php echo $row[3]; ?>"></td>
+  </tr>
+
+  <!--<tr>
+    <td>Phone</td>
+    <td><input id="Theme" name="Phone" type="text" style="width:200px" value="<?php // echo $row[4]; ?>"></td>
+  </tr>-->
+
+</table>
+<?php endwhile; ?>
+
+</div>
+</div> 
+
+<button type='submit' style = 'margin-right: 20px' >Modify</button>
+<button type='submit'>Save</button>
+
+
 </form>
 
-<div id="fb-root"></div>
+
+<div class="information">
+<div class="container">
+<div class = "row">
+
+	<h1 style= "padding-right: 200px;">Current Plans</h1>
+  <table>
+    <tr>
+    <td>Plan 1</td>
+    <td><a href="http://www.rakuten.co.jp">xxx</a></td>
+  </tr>
+	
+  <tr>
+    <td>Plan 2</td>
+    <td><a href="http://www.rakuten.co.jp">yyy</a></td>
+  </tr>
+	
+  <tr>
+    <td>Plan 3</td>
+    <td><a href="http://www.rakuten.co.jp">zzz</a></td>
+  </tr>
+  </table>
+ 
+                
+
+	<h1 style= "padding-right: 250px;">Past Plans</h1>
+  <table>
+  <tr>
+    <td>Plan 1</td>
+    <td><a href="http://www.rakuten.co.jp">xxx</a></td>
+  </tr>
+	
+  <tr>
+    <td>Plan 2</td>
+    <td><a href="http://www.rakuten.co.jp">yyy</a></td>
+  </tr>
+	
+  <tr>
+    <td>Plan 3</td>
+    <td><a href="http://www.rakuten.co.jp">zzz</a></td>
+  </tr>
+
+  </table>
+  
+ </div>
+</div>
+</div>
+
+
+<div class="information">
+<div class="container">
+<h1 style= "padding-right: 200px;">Box of Benefits</h1>
+  <table>
+    <tr>
+    <td>Benefit 1</td>
+    <td><a href="http://www.rakuten.co.jp">xxx</a></td>
+  </tr>
+	
+  <tr>
+    <td>Benefit 2</td>
+    <td><a href="http://www.rakuten.co.jp">yyy</a></td>
+  </tr>
+	
+  <tr>
+    <td>Benefit 3</td>
+    <td><a href="http://www.rakuten.co.jp">zzz</a></td>
+  </tr>
+  </table>
+</div>
+</div>
 
 </body>
+</center>
+</html>
